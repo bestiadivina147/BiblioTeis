e5ffaef00b3c06ac1203bd6428f5671bf7022487
bestiadivina147 <diegopc147@gmail.com>
2025-04-03 11:28:49 +0200

    cambios en codigo para la toolbar

--- app/src/main/java/com/example/biblioteis/LibroActivity.java
+++ app/src/main/java/com/example/biblioteis/LibroActivity.java
@@ -1,10 +1,17 @@
 package com.example.biblioteis;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
+import androidx.core.view.MenuProvider;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
--- app/src/main/java/com/example/biblioteis/MainActivity.java
+++ app/src/main/java/com/example/biblioteis/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.biblioteis;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
+import androidx.core.view.MenuProvider;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
@@ -25,4 +32,40 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
     }
+
+    private void setupToolbar(AppCompatActivity activity) {
+        Toolbar toolbar = activity.findViewById(R.id.libroToolbar);
+        activity.setSupportActionBar(toolbar);
+        activity.addMenuProvider(new MenuProvider() {
+            @Override
+            public void onCreateMenu(@NonNull Menu menu, @NonNull MenuInflater menuInflater) {
+                menuInflater.inflate(R.menu.menu_toolbar, menu);
+                menu.findItem(R.id.opcion_inicio).setVisible(false);
+            }
+
+            @Override
+            public boolean onMenuItemSelected(@NonNull MenuItem menuItem) {
+                if (menuItem.getItemId() == R.id.cerrar_sesion) {
+                    activity.startActivity(new Intent(activity, MainActivity.class)
+                            .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK));
+                    return true;
+                }
+
+                if (menuItem.getItemId() == R.id.opcion_usuario) {
+                    activity.startActivity(new Intent(activity, UsuarioActivity.class)
+                            .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK));
+                    return true;
+                }
+                if (menuItem.getItemId() == R.id.opcion_libreria) {
+                    activity.startActivity(new Intent(activity, LibreriaActivity.class)
+                            .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK));
+                    return true;
+                }
+
+                return false;
+            }
+        });
+    }
+
+
 }
