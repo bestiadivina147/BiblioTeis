4e6ff949f940b947a5fd3810f94d6e78802dcacc
bestiadivina147 <diegopc147@gmail.com>
2025-04-09 13:46:13 +0200

    feat():changes in Libro.java

--- app/src/main/java/com/example/biblioteis/models/Libro.java
+++ app/src/main/java/com/example/biblioteis/models/Libro.java
@@ -3,12 +3,14 @@ package com.example.biblioteis.models;
 import java.sql.Date;
 
 public class Libro {
-    public String titulo;
-    public String autor;
-    public Date fechaPublicacion;
-    public String isbn;
-    public boolean estado;
-    public String imagen;
+    private String titulo;
+    private String autor;
+    private Date fechaPublicacion;
+    private String isbn;
+    private String usuario;
+    private String imagen;
+    private int disponibles;
+    private int totales;
 
     public String getTitulo() {
         return titulo;
@@ -24,22 +26,6 @@ public class Libro {
 
     public void setAutor(String autor) {
         this.autor = autor;
-    }
-
-    public String getImagen() {
-        return imagen;
-    }
-
-    public void setImagen(String imagen) {
-        this.imagen = imagen;
-    }
-
-    public boolean isEstado() {
-        return estado;
-    }
-
-    public void setEstado(boolean estado) {
-        this.estado = estado;
     }
 
     public Date getFechaPublicacion() {
@@ -58,15 +44,48 @@ public class Libro {
         this.isbn = isbn;
     }
 
-    public Libro(String titulo, String autor, Date fechaPublicacion, String isbn, boolean estado, String imagen) {
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public int getDisponibles() {
+        return disponibles;
+    }
+
+    public void setDisponibles(int disponibles) {
+        this.disponibles = disponibles;
+    }
+
+    public int getTotales() {
+        return totales;
+    }
+
+    public void setTotales(int totales) {
+        this.totales = totales;
+    }
+
+    public Libro(String titulo, String autor, Date fechaPublicacion, String isbn, String usuario, String imagen, int disponibles, int totales) {
         this.titulo = titulo;
         this.autor = autor;
         this.fechaPublicacion = fechaPublicacion;
         this.isbn = isbn;
-        this.estado = estado;
+        this.usuario = usuario;
         this.imagen = imagen;
+        this.disponibles = disponibles;
+        this.totales = totales;
     }
-
 
     public Libro() {
     }
