e1d43f32ae18b085e7d136d19dd0adbd91f23d73
bestiadivina147 <diegopc147@gmail.com>
2025-04-28 10:20:52 +0200

    add api

---
+++ app/src/main/java/com/example/biblioteis/API/repositories/BookRepository.java
@@ -0,0 +1,15 @@
+package com.example.biblioteis.API.repositories;
+
+import com.example.biblioteis.API.ApiClient;
+import com.example.biblioteis.API.ApiService;
+
+public class BookRepository {
+
+    private ApiService apiService;
+
+    public BookRepository() {
+        apiService = ApiClient.getClient().create(ApiService.class);
+    }
+
+    public
+}
