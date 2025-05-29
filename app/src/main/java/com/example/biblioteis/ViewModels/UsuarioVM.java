8c22f9fdc5e437d5a2dab8813c4f07a080cfd938
bestiadivina147 <diegopc147@gmail.com>
2025-05-29 10:30:59 +0200

    add load  to usuario

--- app/src/main/java/com/example/biblioteis/ViewModels/UsuarioVM.java
+++ app/src/main/java/com/example/biblioteis/ViewModels/UsuarioVM.java
@@ -1,25 +1,56 @@
 package com.example.biblioteis.ViewModels;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
+
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.example.biblioteis.API.models.Book;
 import com.example.biblioteis.API.models.User;
 import com.example.biblioteis.API.repository.BookRepository;
+import com.example.biblioteis.API.repository.ImageRepository;
 import com.example.biblioteis.API.repository.UserRepository;
 import com.example.biblioteis.mapper.BookMapper;
+import com.example.biblioteis.mapper.UserMapper;
+import com.example.biblioteis.models.Libro;
 import com.example.biblioteis.models.LibroDetalle;
 import com.example.biblioteis.models.Usuario;
 
+import okhttp3.ResponseBody;
+
 public class UsuarioVM extends ViewModel {
     public MutableLiveData<Usuario> usuarioLD= new MutableLiveData<>();
+    public MutableLiveData<Libro> librosLD= new MutableLiveData<>();
     public UserRepository userRepository = new UserRepository();
+    public BookRepository bookRepository = new BookRepository();
+    public ImageRepository imageRepository = new ImageRepository();
 
     public void load(int id) {
         userRepository.getUserById(id, new BookRepository.ApiCallback<User>() {
             @Override
             public void onSuccess(User result) {
+                Usuario usuario = UserMapper.user2Usuario(result);
+                usuarioLD.setValue(usuario);
+            }
 
+            @Override
+            public void onFailure(Throwable t) {
+                Log.e("UsuarioVM", "Error al obtener usuario", t);
+            }
+        });
+
+    }
+
+
+    public void loadLibro(int id) {
+        bookRepository.getBookById(id, new BookRepository.ApiCallback<Book>() {
+            @Override
+            public void onSuccess(Book result) {
+                Libro libro = BookMapper.book2Libro(result);
+                librosLD.setValue(libro);
+                obtenLibroImagen(result.getBookPicture(), libro);
             }
 
             @Override
@@ -28,5 +59,28 @@ public class UsuarioVM extends ViewModel {
             }
         });
 
+    }
+
+    private void obtenLibroImagen(String urlImg, Libro libro) {
+        if(urlImg==null){
+            return;
+        }
+        imageRepository.getImage(urlImg, new BookRepository.ApiCallback<ResponseBody>() {
+            @Override
+            public void onSuccess(ResponseBody result) {
+                if (result == null){
+                    return;
+                }
+                Bitmap bm = BitmapFactory.decodeStream(result.byteStream());
+                libro.setImg(bm);
+                librosLD.postValue(libro);
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                //TODO
+                Log.e("LibreriaVM", "Error load books", t);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/biblioteis/activities/UsuarioActivity.java
+++ app/src/main/java/com/example/biblioteis/activities/UsuarioActivity.java
@@ -1,18 +1,33 @@
 package com.example.biblioteis.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.biblioteis.R;
+import com.example.biblioteis.ViewModels.DetalleVM;
 import com.example.biblioteis.ViewModels.UsuarioVM;
+import com.example.biblioteis.models.Libro;
+import com.example.biblioteis.models.LibroLending;
 import com.example.biblioteis.utils.IPreferenciasUsuario;
+import com.example.biblioteis.utils.PreferenciasUsuario;
 import com.example.biblioteis.utils.ToolbarUtils;
 
 public class UsuarioActivity extends AppCompatActivity {
@@ -34,5 +49,74 @@ public class UsuarioActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        prefs = new PreferenciasUsuario(this);
+        //VINCULAR ELEMENTOS GRAFICOS
+        txtNombre = findViewById(R.id.txtNombre);
+        txtCorreo = findViewById(R.id.txtCorreo);
+        txtFecha = findViewById(R.id.txtFecha);
+        rv = findViewById(R.id.rvLibrosUsuario);
+        //OBTENER VM
+        vm = new ViewModelProvider(this).get(UsuarioVM.class);
+        //OBSERVAR VM
+        vm.usuarioLD.observe(this,usuario -> {
+            if (usuario != null) {
+                System.out.println("UsuarioActivity" + usuario.getNombre());
+                txtNombre.setText(usuario.getNombre());
+                txtCorreo.setText(usuario.getCorreo());
+                txtFecha.setText(usuario.getFecha());
+            } else {
+                System.out.println("usuarioLD es nulo");
+            }
+
+            rv.setLayoutManager(new LinearLayoutManager(this));
+            rv.setLayoutManager(new LinearLayoutManager(this));
+            rv.setAdapter(new RecyclerView.Adapter() {
+
+                class LibroViewHolder extends RecyclerView.ViewHolder {
+
+                    public TextView tvAutor, tvTitulo, tvFecha, tvDisponibles, tvTotales;
+                    public ImageView imgLibro;
+                    public Button btnVerMas;
+
+                    public ConstraintLayout cl;
+
+                    public LibroViewHolder(@NonNull View iv) {
+                        super(iv);
+
+                        tvAutor = iv.findViewById(R.id.tvFrAutor);
+                        tvTitulo = iv.findViewById(R.id.tvFrTitulo);
+                        tvFecha = iv.findViewById(R.id.tvFrFecha);
+                        tvDisponibles = iv.findViewById(R.id.tvFrDisponibles);
+                        tvTotales = iv.findViewById(R.id.tvFrTotales);
+
+                        imgLibro = iv.findViewById(R.id.imgFrLibro);
+                        cl = iv.findViewById(R.id.libroConstraint);
+                        btnVerMas = iv.findViewById(R.id.btnVerMas);
+                    }
+                }
+
+                @NonNull
+                @Override
+                public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                    View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_libro, parent, false);
+
+                    return new LibroViewHolder(view);
+                }
+
+                @Override
+                public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+                }
+
+                @Override
+                public int getItemCount() {
+                    return 0;
+                }
+            });
+        });
+
+        //VINCULAR ACCIONES
+        int id = prefs.leer();
+        vm.load(id);
     }
 }
